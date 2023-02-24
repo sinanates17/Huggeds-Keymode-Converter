99f814fff3427576bee44b252872a70127410df5
Hugged <sinanates17@gmail.com>
2023-02-23 19:01:31 -0500

    Defined an update function

--- update.py
+++ update.py
@@ -3,26 +3,24 @@ from zipfile import ZipFile
 import shutil
 import os
 
-url = "http://github.com/sinanates17/Huggeds-Keymode-Converter/releases/latest"
-r = requests.get(url, allow_redirects=True)
-version = r.url.split('/')[-1]
-dl_link = "https://github.com/sinanates17/Huggeds-Keymode-Converter/archive/refs/tags/" + version + ".zip"
-dl = requests.get(dl_link, allow_redirects=True)
+def update(newVersion)
+    dl_link = "https://github.com/sinanates17/Huggeds-Keymode-Converter/archive/refs/tags/" + newNersion + ".zip"
+    dl = requests.get(dl_link, allow_redirects=True)
 
-os.makedirs("delete")
+    os.makedirs("delete")
 
-for oldFile in os.listdir():
-    shutil.move(oldFile, "delete/" + oldFile)
+    for oldFile in os.listdir():
+        shutil.move(oldFile, "delete/" + oldFile)
 
-open("temp.zip","wb").write(dl.content)
+    open("temp.zip","wb").write(dl.content)
 
-with ZipFile("temp.zip",'r') as temp:
-    temp.extractall()
-    temp.close()
+    with ZipFile("temp.zip",'r') as temp:
+        temp.extractall()
+        temp.close()
 
-shutil.move("temp.zip", "delete/temp.zip")
+    shutil.move("temp.zip", "delete/temp.zip")
 
-for newFile in os.listdir("Huggeds-Keymode-Converter-" + version):
-    shutil.move(newFile, os.getcwd() + "/" + newFile.split("/")[-1])
+    for newFile in os.listdir("Huggeds-Keymode-Converter-" + newVersion):
+        shutil.move(newFile, os.getcwd() + "/" + newFile.split("/")[-1])
 
-print("Update Complete!")
+    print("Update Complete! Please press any key to exit then rerun the program.")
