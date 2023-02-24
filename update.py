0d551eb52a45fcebc91d4b2b77aafe8b7572ab57
Hugged <sinanates17@gmail.com>
2023-02-23 21:32:09 -0500

    updater script now functional

--- update.py
+++ update.py
@@ -12,17 +12,30 @@ def updater(newVersion):
             shutil.rmtree(oldFile)
         else:
             os.remove(oldFile)
+        #print("Removed " + oldFile)
 
     open("temp.zip","wb").write(dl.content)
+    #print("Created temp zip")
 
-    with ZipFile(oneFolderUp + "temp.zip",'r') as temp:
+    with ZipFile("temp.zip",'r') as temp:
         temp.extractall()
+        #print("Extracted temp zip")
 
     os.remove("temp.zip")
+    #print("Removed temp zip")
 
     for newFile in os.listdir(os.getcwd() + "/Huggeds-Keymode-Converter-" + newVersion):
-        shutil.copy(os.getcwd() + "/Huggeds-Keymode-Converter-" + newVersion + "/" + newFile, os.getcwd() + "/" + newFile)
+        path = os.getcwd() + "/Huggeds-Keymode-Converter-" + newVersion + "/" + newFile
+        if os.path.isdir(path):
+            #print("Trying to copy " + newFile + " as a directory")
+            shutil.copytree(os.getcwd() + "/Huggeds-Keymode-Converter-" + newVersion + "/" + newFile, os.getcwd() + "/" + newFile)
+        else:
+            #print("Trying to copy " + newFile + " as a file")
+            shutil.copy(os.getcwd() + "/Huggeds-Keymode-Converter-" + newVersion + "/" + newFile, os.getcwd() + "/" + newFile)
+                    
+        #print("Copied " + newFile + " to root dir")
                     
     shutil.rmtree(os.getcwd() + "/Huggeds-Keymode-Converter-" + newVersion)
+    #print("Removed extracted dir after copying")
 
     print("Update Complete! Please press any key to exit then rerun the program.")
