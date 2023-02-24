fc71273b611bab4967e5817ec53119ca85aa2ef7
Konomi <bennett@oortnetworks.com.au>
2023-02-24 12:18:07 +1100

    Update and rename update.py to updater.py

--- Converter/updater.py
+++ Converter/updater.py
@@ -3,7 +3,26 @@ from zipfile import ZipFile
 import shutil
 import os
 
-def updater(newVersion):
+def checkUpdate(currentVersion):
+    #Check if the current verison is up to date, if so, ask the user if they want to auto update
+    currentVersion = "osu" #Change this line every time a new release is made
+    url = "http://github.com/sinanates17/Huggeds-Keymode-Converter/releases/latest"
+    r = requests.get(url, allow_redirects=True)
+    latestVersion = r.url.split('/')[-1]
+
+    if currentVersion != latestVersion:
+        choice = input("A new release is available, would you like to update? (\"yes\" or \"no\") ").lower()
+        while choice not in ["yes", "no"]:
+            choice = input("\"yes\" or \"no\" ").lower()
+        if choice == "yes":
+            import sys
+            updater(latestVersion)
+            input()
+            sys.exit()
+        elif choice == "no":
+            pass
+
+def update(newVersion):
     dl_link = "https://github.com/sinanates17/Huggeds-Keymode-Converter/archive/refs/tags/" + newVersion + ".zip"
     dl = requests.get(dl_link, allow_redirects=True)
 
