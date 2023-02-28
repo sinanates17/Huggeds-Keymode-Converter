fe53fc988d191db90ac0d38a1344067504cb04c5
Hugged <sinanates17@gmail.com>
2023-02-27 23:54:24 -0500

    autosort, .osz and song folder processing
    didnt test yet

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import convert
 import parsing
 import updater
 import pip._vendor.requests as requests
+import zipfile
+import shutil
 
 currentVersion = "osu" #Change this line every time a new release is made
 
@@ -19,6 +21,8 @@ changeHP = cfg['Metadata'].get('change_hp')
 if changeHP == '': changeHP = None
 changeOD = cfg['Metadata'].get('change_od')
 if changeOD == '': changeOD = None
+autoSort = cfg['Metadata']['autosort']
+songDir = cfg['Metadata']['songs_folder']
 
 outputKeymode = cfg.getint('Mapping','target_keymode')
 conversionMode = cfg['Mapping']['conversion_mode']
@@ -37,28 +41,50 @@ buffAmount = cfg.getfloat('Mapping','buff')
 inputDirectory = os.getcwd() + '/Input/'
 outputDirectory = os.getcwd() + '/Output/'
 
-beatmaps = []
+#Parse the Input folder for maps to process
+beatmaps = [] #Formated as [beatmap, origin]
 for f in os.listdir(inputDirectory):
     # Skip if not file
-    if not os.path.isfile(inputDirectory + f):
-        continue
+    #if not os.path.isfile(inputDirectory + f):
+        #continue
 
     # Add all .osu files
     if f.endswith(".osu"):
-        beatmaps.append(f)
+        beatmaps.append([f, None])
 
-    # Skip .osz for now, maybe add support later
+    # If an osz is inputted, scan it and add any .osu files to the beatmaps
     elif f.endswith(".osz"):
-        pass
+        #Extract the .osz into a temporary folder
+        with zipfile.ZipFile(inputDirectory + f, 'r') as osz:
+            os.mkdir(inputDirectory + f[0:-4])
+            osz.extractall(path=inputDirectory + f[0:-4])
+        for ff in os.listdir(inputDirectory + f[0:-4]):
+            if ff.endswith(".osu"):
+                beatmaps.append([ff, inputDirectory + f])
+        
+    # If a beatmap folder is inputted, scan it and add any .osu files to the beatmaps
+    elif os.path.isdir(inputDirectory + f):
+        for ff in os.listdir(os.getcwd() +  f):
+            if ff.endswith(".osu"):
+                beatmaps.append([ff, inputDirectory + f])
 
-# Loop through every difficulty in the Input folder and create converts in the Output folder
+# Loop through every difficulty in the Input folder and create converts
 for i,beatmap in enumerate(beatmaps):
     #Print progress
-    print(f'Converting map {i+1}/{len(beatmaps)} | {beatmap.strip(".osu")}')
+    print(f'Converting map {i+1}/{len(beatmaps)} | {beatmap[0].strip(".osu")}')
 
     # Parse the map for its stuff
-    with open(inputDirectory + beatmap, "r", encoding="utf8") as f:
-        inputKeymode, redPoints, hitObjects, outputHead = parsing.parseMap(f, changeAuthor, changeHP, changeOD)
+    if beatmap[1] == None:
+        with open(inputDirectory + beatmap[0], "r", encoding="utf8") as f:
+            inputKeymode, redPoints, hitObjects, outputHead, setID, creator, title, artist = parsing.parseMap(f, changeAuthor, changeHP, changeOD)
+    
+    elif '.osz' in beatmap[1]:
+        with open(beatmap[1][0:-4] + "/" + beatmap[0], "r", encoding="utf8") as f:
+            inputKeymode, redPoints, hitObjects, outputHead, setID, creator, title, artist = parsing.parseMap(f, changeAuthor, changeHP, changeOD)
+
+    elif os.path.isdir(beatmap[1]):
+        with open(inputDirectory + beatmap[1] + "/" + beatmap[0], "r", encoding="utf8") as f:
+            inputKeymode, redPoints, hitObjects, outputHead, setID, creator, title, artist = parsing.parseMap(f, changeAuthor, changeHP, changeOD)
 
     # Parse the conversion key
     conversionKey = parsing.parseConversionKey(cfg['Conversion Keys'], inputKeymode, outputKeymode)
@@ -66,18 +92,77 @@ for i,beatmap in enumerate(beatmaps):
     # Pass all of the timing points, hit objects, and configurable paramaters into the 'convert' method, which returns a list containing all of the hit objects for the output file
     newHitObjects = convert.convertMap(hitObjects, redPoints, inputKeymode, outputKeymode, conversionMode, alternateInterval, maxJack, unjack, unjackInterval, minShieldInterval, conversionKey, shieldPreserver, shieldThreshold, shieldPreserverInterval, maxShield, buffAmount)
 
-    # Start writing metadata into the output difficulty
-    filename = beatmap[0:-4] + " To " + str(outputKeymode) + "K.osu" #Set the filename for the converted difficulty
-    with open(outputDirectory + filename,"w", encoding="utf8") as f:
-        #todo changeAuthor
-        #todo changeHP
-        #todo changeOD
-        edits = {'keymode': outputKeymode}
-        f.write(outputHead.format(**edits))
+    filename = beatmap[0][0:-4] + " To " + str(outputKeymode) + "K.osu" #Set the filename for the converted difficulty
 
-        # Code to write the new hit objects into the output file
-        for newNote in newHitObjects:
-            laneNumber = (newNote.lane)*(512/outputKeymode)+2
-            f.write(f'{laneNumber},192,{newNote.startTime},{newNote.noteType},{newNote.hitSound},{newNote.endTime}{newNote.sample}\n')
+    #Try to find the right beatmap folder if autosort is on
+    destination = None
+    if autoSort == '1':
+        for folder in os.listdir(songDir):
+            done = False
+            if os.path.isdir(songDir + '/' + folder):
+                if title in folder and artist in folder:
+                    for file in os.listdir(songDir + '/' + folder):
+                        if file.endswith('.osu'):
+                            with open(songDir + '/' + folder + '/' + file) as chart:
+                                while True:
+                                    line = chart.readline()
+                                    if 'Title:' in line:
+                                        theTitle = line.split(':')[-1]
+                                    elif 'Artist:' in line:
+                                        theArtist = line.split(':')[-1]
+                                    elif 'Creator:' in line:
+                                        theCreator = line.split(':')[-1]
+                                    elif 'BeatmapSetID:' in line:
+                                        theSetID = line.split(':')[-1]
+                                        break
+                            if theSetID == setID and setID != '-1':
+                                done = True
+                                destination = songDir + '/' + folder + '/'
+                                break
+                            elif title == theTitle and artist == theArtist and creator == theCreator and setID == '-1':
+                                done = True
+                                destination = songDir + '/' + folder + '/'
+                                break
+                    if done:
+                        break
+
+    #Move the converted .osus into the proper beatmap folders 
+    if destination != None or beatmap[1] == None:
+        with open(destination + filename,"w", encoding="utf8") as f:
+            #todo changeAuthor
+            #todo changeHP
+            #todo changeOD
+            edits = {'keymode': outputKeymode}
+            f.write(outputHead.format(**edits))
+
+            # Code to write the new hit objects into the output file
+            for newNote in newHitObjects:
+                laneNumber = (newNote.lane)*(512/outputKeymode)+2
+                f.write(f'{laneNumber},192,{newNote.startTime},{newNote.noteType},{newNote.hitSound},{newNote.endTime}{newNote.sample}\n')
+    else:
+        with open(outputDirectory + filename,"w", encoding="utf8") as f:
+            #todo changeAuthor
+            #todo changeHP
+            #todo changeOD
+            edits = {'keymode': outputKeymode}
+            f.write(outputHead.format(**edits))
+
+            # Code to write the new hit objects into the output file
+            for newNote in newHitObjects:
+                laneNumber = (newNote.lane)*(512/outputKeymode)+2
+                f.write(f'{laneNumber},192,{newNote.startTime},{newNote.noteType},{newNote.hitSound},{newNote.endTime}{newNote.sample}\n')
+
+    if os.path.isdir(beatmap[1]):
+        shutil.move(outputDirectory + filename, destination)
+
+    elif '.osz' in beatmap[1]:
+        with zipfile.ZipFile(beatmap[1],"a") as dest:
+            dest.write(outputDirectory + filename, filename)
+            os.remove(outputDirectory + filename)
+
+#After everything, move .oszs and beatmap folders in the Input folder to the Output folder
+for dir in os.listdir(inputDirectory):
+    if '.osz' in dir or os.path.isdir(inputDirectory + dir):
+        shutil.move(inputDirectory + dir, outputDirectory)
 
 input("Done! Press enter to exit.")
